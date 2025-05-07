bacf6c7fa9c4e2c4fc67695beaeb8fc9818a13f9
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-08 00:38:10 +0600

    the 2nd problem sloved

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,26 @@ function formatString(input: string, toUpper?: boolean): string {
   }
 }
 
-console.log(formatString("Hello"));
-console.log(formatString("Hello", true));
-console.log(formatString("Hello", false));
+// console.log(formatString("Hello"));
+// console.log(formatString("Hello", true));
+// console.log(formatString("Hello", false));
 
 //-----------------------------------------------------------------------------------------
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter(
+    (value: { title: string; rating: number }) => value.rating > 4
+  );
+}
+
+console.log(filterByRating(books));
 //-----------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------
